1c464cef616aa2e50ffca04aaff040d2e54f1847
DanishSofii <bashirdanish85@gmail.com>
2024-03-07 15:53:48 +0530

    sw changes

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,25 @@
 let cacheData = "appV1";
-this.addEventListener("install",(event)=>{
-    event.waitUntil(
-        caches.open(cacheData).then((cache)=>{
-            cache.addAll([
-                "/static/js/main.chunk.js",
-                "/static/js/0.chunk.js",
-                "/static/js/bundle.js",
-                "/index.html",
-                "/"
 
-            ])
-        })
-    )
-})
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+       cache.addAll([
+        "/static/js/main.chunk.js",
+        "/static/js/0.chunk.js",
+        "/static/js/bundle.js",
+        "/static/js/main.77642b16.js",
+        "/static/css/main.c4934f97.css",
+        "/static/js/main.0ec8a9fc.js.map",
+        "/manifest.json",
+        "/index.html",
+        "/",
+        "/Footer",
+        "/Header",
+        "/Messages"
+      ]);
+    })
+  );
+});
 
 this.addEventListener("fetch",(event)=>{
     if(!navigator.onLine){
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import swDev from './swDev';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+// serviceWorkerRegistration.register();
 swDev();
